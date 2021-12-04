830a6c4b2e4881e7d942c02934dc8c296586f0ef
jiawei <65395926+jiaweizh-823@users.noreply.github.com>
2021-12-04 16:46:01 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+# def read_default_dataset(filepath=r'project2\Inquiry-System-For-Taiwan-Traffic-Data-master\data\default.csv'):
+#     df = pd.read_csv(filepath, header=None)
+#     df.columns = ['VehicleType',
+#                   'DerectionTime_O',
+#                   'GantryID_O',
+#                   'DerectionTime_D',
+#                   'GantryID_D',
+#                   'TripLength',
+#                   'TripEnd',
+#                   'TripInformation']
+#     return df
+
+# traffic_df = read_default_dataset()
+
+def search(df, column, keyword):
+    r = df[df[column].isin([keyword])]
+    if r.empty:
+        return df[df[column].str.contains(keyword)]
+    return r
+
+def sort(df, column, way, num):
+    if way == 'ascending':
+        acd = True
+    elif way == 'non-ascending':
+        acd = False
+    return df.sort_values(by=[column], ascending=acd)[:num]
+# print(search(traffic_df, 'VehicleType', 31))
